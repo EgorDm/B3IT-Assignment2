f93b607f55e20d5620ac1bd9725ce9ad233e0e79
Egor Dmitriev <egordmitriev2@gmail.com>
2018-03-17 13:52:27 +0100

    Added histogram struct

--- cvision/image_processing.h
+++ cvision/image_processing.h
@@ -16,7 +16,28 @@ namespace cvision { namespace processing { namespace image {
         const float *ranges;
         const cv::Mat *channels;
 
-    Scalar extract_dominant_color(const std::vector<Mat> &histogram);
+        Histogram() : channel_count(0), ranges(nullptr), channels(nullptr) {};
+
+        explicit Histogram(cv::Mat *channels, const int channel_count, const float *channel_ranges)
+                : channel_count(channel_count), channels(channels), ranges(channel_ranges) {
+            for (int i = 0; i < channel_count; ++i) {
+                assert(sum(channels[i])[0] - 1 < EPSILON);
+            }
+        }
+
+        virtual ~Histogram() {
+            delete[] channels;
+            delete[] ranges;
+        }
+
+        float probability(const double value[]) const;
+
+        float intensity(int channel, double value) const;
+
+        int bin_count() const { return channels[0].rows; }
+
+        const cv::Mat &operator[](const unsigned int i) const { return channels[i]; }
+    };
 }}}
 
 #endif //B3ITASSIGNMENT2_IMAGE_PROCESSING_H
