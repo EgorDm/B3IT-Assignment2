de1e599f1ccf620c0a8614fccb8079ddaf013709
Egor Dmitriev <egordmitriev2@gmail.com>
2018-03-20 22:52:15 +0100

    Added the remaining sensors

--- device/tasks.cpp
+++ device/tasks.cpp
@@ -10,7 +10,6 @@ bool MQTTReconnectCycle::execute() {
     SHOUT("Connecting to MQTT broker...");
     if (client.connect("", config.mqtt_username, config.mqtt_password)) {
         SHOUT("Succesfully connected to MQTT broker.");
-        client.publish("outTopic", "hello world");
     } else {
         SHOUT("Failexd to connect to MQTT broker. State: " + String(client.state()));
     }
@@ -24,9 +23,15 @@ bool MQTTReconnectCycle::should_run(unsigned long time) {
 bool BME20SensorRoutine::execute() {
     if (!started) return true;
 
-    client.publish(TOPIC("/sensor/temperature"), String(bme.readTemperature()).c_str(), false);
-    client.publish(TOPIC("/sensor/pressure"), String(bme.readPressure() / 100.0F).c_str(), false);
-    client.publish(TOPIC("/sensor/humidity"), String(bme.readHumidity()).c_str(), false);
+    client.publish(TOPIC("/sensor/temperature"), String(bme.readTemperature()).c_str(), true);
+    client.publish(TOPIC("/sensor/pressure"), String(bme.readPressure() / 100.0F).c_str(), true);
+    client.publish(TOPIC("/sensor/humidity"), String(bme.readHumidity()).c_str(), true);
+
+    digitalWrite(SPB_SEL, HIGH);
+    client.publish(TOPIC("/sensor/soil_moisture"), String(analogRead(SPB_ANALOG)).c_str(), true);
+
+    digitalWrite(SPB_SEL, LOW);
+    client.publish(TOPIC("/sensor/light"), String(analogRead(SPB_ANALOG)).c_str(), true);
 
     return false;
 }
