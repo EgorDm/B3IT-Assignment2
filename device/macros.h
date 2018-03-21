d32da934ca62cbbd472b936b4c3d4945e4dc8aad
Egor Dmitriev <egordmitriev2@gmail.com>
2018-03-21 09:37:43 +0100

    Fixed macros

--- device/macros.h
+++ device/macros.h
@@ -12,4 +12,6 @@
 #define REDIRECT(url) \
     server.sendHeader("Location", (url), true); server.send(302, "text/plain", "");
 
+#define TOPIC(topic) (MQTT_ROOT_TOPIC + (topic)).c_str()
+
 #endif //MACROS_H
