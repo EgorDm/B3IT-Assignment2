2e099c935f5d621e39bbb3ffa30a018ce0507ee7
Egor Dmitriev <egordmitriev2@gmail.com>
2018-04-09 13:53:13 +0200

    Added hand stabilization by tracking it and averaging positions

---
+++ main/detection_helper.cpp
@@ -0,0 +1,150 @@
+//
+// Created by egordm on 22-3-2018.
+//
+
+#include "detection_helper.h"
+#include "../cvision/math.hpp"
+#include "../cvision/hand_recognition.h"
+#include "../cvision/visualization.h"
+
+using namespace cvision;
+using namespace cv;
+
+
+cv::Mat HandDetectorHelper::draw(const cv::Mat &src, const cv::Mat &original) {
+    Mat mask_final;
+    mask.copyTo(mask_final);
+
+    if (faces != nullptr && !faces->empty()) {
+        for (const auto &face : *faces) {
+            Point center(face.x + face.width / 2, face.y + face.height / 2);
+            ellipse(mask_final, center, Size(face.width / 2, face.height / 2), 0, 0, 360, Scalar(0, 0, 0), -1);
+        }
+    }
+
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(mask_final, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+    contours.erase(std::remove_if(contours.begin(), contours.end(), [](const std::vector<cv::Point> &contour) {
+        return cv::contourArea(contour) < MIN_HAND_AREA;
+    }), contours.end());
+
+    hands.clear();
+    for (const auto &contour : contours) {
+        if(contour.size() < 3) continue;
+        hands.push_back(processing::limb_recognition::hand::recognize_hand(contour));
+    }
+
+    return src;
+}
+
+FaceDetectorHelper::FaceDetectorHelper() {
+    std::stringstream cascade_name;
+    cascade_name << OPENCV_DATASET_PATH << FACE_CASCADE_NAME;
+    if (!face_cascade.load(cascade_name.str())) {
+        inited = false;
+        std::cout << "Failed loading face dataset!" << std::endl;
+    }
+}
+
+Mat FaceDetectorHelper::draw(const cv::Mat &src, const cv::Mat &original) {
+    if (!inited) return src;
+    Mat frame_gray;
+    faces.clear();
+
+    cvtColor(original, frame_gray, COLOR_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
+
+    face_cascade.detectMultiScale(frame_gray, faces, 1.3, 2, 0 | CASCADE_SCALE_IMAGE, Size(35, 35));
+
+    for (const auto &face : faces) {
+        Point center(face.x + face.width / 2, face.y + face.height / 2);
+        ellipse(src, center, Size(face.width / 2, face.height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+    }
+
+    return src;
+}
+
+#define DISTANCE_THRESH (min_dim * 6.0)
+
+
+cv::Mat HandTrackingHelper::draw(const cv::Mat &src, const cv::Mat &original) {
+    const auto &hands = hands_detector->hands;
+    const int min_dim = min(original.rows, original.cols);
+
+    hand_tracker.distance_threshold = DISTANCE_THRESH;
+    finger_tracker.distance_threshold = DISTANCE_THRESH/2;
+    hand_tracker.update(hands, hand_histories);
+
+    for (const auto &history : hand_histories) {
+        finger_tracker.update(history->state_history.back().fingers, history->finger_histories);
+
+        std::stringstream ss;
+        ss << history->identifier;
+
+        auto hand = average_hand(history);
+        circle(src, hand.palm_center, (int) hand.palm_radius, cv::Scalar(255, 255, 255), 2);
+        circle(src, hand.enclosing_center, (int) hand.enclosing_radius, cv::Scalar(255, 255, 200), 2);
+        cv::putText(src, ss.str(), hand.palm_center, cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(255, 255, 0));
+
+        for(const auto &finger_history : history->finger_histories) {
+            auto finger = average_finger(finger_history);
+            std::stringstream sz;
+            sz << finger_history->identifier;
+
+            circle(src, finger.tip + hand.palm_center, 2, cv::Scalar(0, 0, 255), 2);
+            line(src, finger.tip - finger.direction/2 + hand.palm_center, finger.tip + finger.direction/2 + hand.palm_center, cv::Scalar(255, 0, 0), 2);
+            line(src, finger.tip + hand.palm_center, hand.palm_center, cv::Scalar(0, 255, 255), 2);
+            cv::putText(src, sz.str(), finger.tip + hand.palm_center, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 255));
+        }
+    }
+
+    return WindowHelper::draw(src, original);
+}
+
+double HandTrackingHelper::distance_hand(const Hand &state, const HandHistory *history) {
+    return math::distance_sq(state.palm_center, history->state_history.back().palm_center);
+}
+
+double HandTrackingHelper::distance_finger(const Finger &state, const TrackHistory<Finger> *history) {
+    return math::distance_sq(state.tip, history->state_history.back().tip);
+}
+
+Hand HandTrackingHelper::average_hand(const HandHistory * history, const int &lookup_length) {
+    Hand ret;
+    int lookup_count = min((int)history->state_history.size(), lookup_length);
+    int lookup_till = (int)history->state_history.size() - lookup_count;
+    for(int i = (int)history->state_history.size() - 1; i >= lookup_till; --i) {
+        auto state = history->state_history[i];
+        ret.enclosing_center += state.enclosing_center;
+        ret.palm_center += state.palm_center;
+        ret.enclosing_radius += state.enclosing_radius;
+        ret.palm_radius += state.palm_radius;
+        ret.palm_to_hand_ratio += state.palm_to_hand_ratio;
+    }
+    ret.enclosing_center /= lookup_count;
+    ret.palm_center /= lookup_count;
+    ret.enclosing_radius /= lookup_count;
+    ret.palm_radius /= lookup_count;
+    ret.palm_to_hand_ratio /= lookup_count;
+
+    return ret;
+}
+
+Finger HandTrackingHelper::average_finger(const TrackHistory<Finger> * history, const int &lookup_length) {
+    cv::Point tip;
+    cv::Point direction;
+    bool thumb;
+
+    int lookup_count = min((int)history->state_history.size(), lookup_length);
+    int lookup_till = (int)history->state_history.size() - lookup_count;
+    for(int i = (int)history->state_history.size() - 1; i >= lookup_till; --i) {
+        auto state = history->state_history[i];
+        tip += state.tip;
+        direction += state.direction;
+        thumb = state.thumb;
+    }
+    tip /= lookup_count;
+    direction /= lookup_count;
+
+    return {tip, direction, thumb};
+}
---
+++ main/detection_helper.h
@@ -0,0 +1,92 @@
+//
+// Created by egordm on 22-3-2018.
+//
+
+#ifndef B3ITASSIGNMENT2_GESTURE_DETECTION_HELPER_H
+#define B3ITASSIGNMENT2_GESTURE_DETECTION_HELPER_H
+
+
+#include "window.h"
+#include "../cvision/defines.h"
+#include "../cvision/hand_recognition.h"
+#include "../cvision/object_tracking.h"
+
+#define MIN_HAND_AREA 4000
+// Tracking
+#define HAND_HISTORY_SIZE 3
+#define FINGER_HISTORY_SIZE 5
+#define DISTANCE_THRESH (min_dim * 6.0)
+#define HAND_TIMEOUT_TICKS 5
+#define FINGER_TIMEOUT_TICKS 5
+
+using namespace cvision::processing::limb_recognition::hand;
+using namespace cvision::processing::tracking;
+
+class HandDetectorHelper : public WindowHelper {
+private:
+    const cv::Mat &mask;
+    const std::vector<cv::Rect> *faces;
+public:
+    std::vector<cvision::processing::limb_recognition::hand::Hand> hands;
+    explicit HandDetectorHelper(const cv::Mat &mask, const std::vector<cv::Rect> *faces = nullptr) : mask(mask), faces(faces) {}
+
+    cv::Mat draw(const cv::Mat &src, const cv::Mat &original) override;
+};
+
+
+#define FACE_CASCADE_NAME "haarcascade_frontalface_alt.xml"
+
+class FaceDetectorHelper : public WindowHelper {
+private:
+    cv::CascadeClassifier face_cascade;
+    bool inited = true;
+
+public:
+    std::vector<cv::Rect> faces;
+
+    FaceDetectorHelper();
+
+    cv::Mat draw(const cv::Mat &src, const cv::Mat &original) override;
+};
+
+struct HandHistory : public TrackHistory<Hand> {
+    std::vector<TrackHistory<Finger>*> finger_histories;
+
+    HandHistory(const Hand &o, int identifier) : TrackHistory(o, identifier) {}
+
+    virtual ~HandHistory() {
+        for(auto &finger : finger_histories) delete finger;
+        finger_histories.clear();
+    }
+};
+
+class HandTrackingHelper : public WindowHelper {
+private:
+    const HandDetectorHelper *hands_detector;
+    ObjectTracker<Hand, HandHistory> hand_tracker;
+    ObjectTracker<Finger, TrackHistory<Finger>> finger_tracker;
+public:
+    std::vector<HandHistory*> hand_histories;
+
+    explicit HandTrackingHelper(const HandDetectorHelper *hands_detector)
+            : hands_detector(hands_detector), hand_tracker(HAND_HISTORY_SIZE, HAND_TIMEOUT_TICKS, 0, &distance_hand),
+              finger_tracker(FINGER_HISTORY_SIZE, FINGER_TIMEOUT_TICKS, 0, &distance_finger){}
+
+    virtual ~HandTrackingHelper() {
+        for(auto &hand : hand_histories) delete hand;
+        hand_histories.clear();
+    }
+
+    cv::Mat draw(const cv::Mat &src, const cv::Mat &original) override;
+
+    static double distance_hand(const Hand &state, const HandHistory* history);
+
+    static double distance_finger(const Finger &state, const TrackHistory<Finger>* history);
+
+    static Hand average_hand(const HandHistory* history, const int &lookup_length = HAND_HISTORY_SIZE);
+
+    static Finger average_finger(const TrackHistory<Finger>* history, const int &lookup_length = FINGER_HISTORY_SIZE);
+};
+
+
+#endif //B3ITASSIGNMENT2_GESTURE_DETECTION_HELPER_H
